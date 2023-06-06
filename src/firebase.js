99caaf684013f34699c139c560ac9064c73012be
Vibhor <ridervibhor1999@gmail.com>
2023-06-06 18:09:52 +0530

    fix oauth issue

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcPSKlYtpdzBoAC8soeSmIARMzVKzrf5I",
-  authDomain: "challenge-4b2b2.firebaseapp.com",
-  databaseURL: "https://challenge-4b2b2.firebaseio.com",
-  projectId: "challenge-4b2b2",
-  storageBucket: "challenge-4b2b2.appspot.com",
-  messagingSenderId: "962418448875",
-  appId: "1:962418448875:web:f6cce5eeaf819481f661ae",
+  apiKey: "AIzaSyBX0HA3ID0zjWRGy_Lfj-epLX6prVockls",
+  authDomain: "clone-58ebe.firebaseapp.com",
+  projectId: "clone-58ebe",
+  storageBucket: "clone-58ebe.appspot.com",
+  messagingSenderId: "804250606023",
+  appId: "1:804250606023:web:1b701f30615fd405a7a1e2",
+  measurementId: "G-B2LDQ039K4"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
